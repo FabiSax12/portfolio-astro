0be94dfcb242bf440dd97f24269ac3bc1203628c
Fabián Vargas <fabidev18@gmail.com>
2025-03-31 10:06:59 -0600

    refactor: simplify theme handling and integrate Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  output: "server"
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
